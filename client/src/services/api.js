33bcb5fb3a553328468342dc80899c837469e3f0
Lavonda Maxwell <lavondamaxwell1@gmail.com>
2026-05-14 00:26:31 -0400

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,26 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const savedUser = localStorage.getItem("user");
+api.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user"));
 
-    if (savedUser) {
-      const user = JSON.parse(savedUser);
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
 
-      if (user?.token) {
-        config.headers.Authorization = `Bearer ${user.token}`;
-      }
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+  return config;
+});
 
 export default api;
